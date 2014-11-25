56d0a62f5c927279a6a13b8b06f54751d85b59d2
Tom Savage <tcsavage@gmail.com>
2014-11-25 18:39:31 +0000

    Helper functions for recursive and native functions. Native filter function

--- src/fifth/core.clj
+++ src/fifth/core.clj
@@ -2,12 +2,23 @@
 
 ;; Functions have the rough type of ((Stack, Interpreter) -> Stack).
 
+(defn recursive
+  "Helper function for operating on the stack allowing for re-invoking the interpreter."
+  [f]
+  (fn [s interpret scope]
+    [(f s (partial interpret scope)) scope]))
+
 (defn pure
   "Helper for functions operating solely on the stack.
   Lifts a function (Stack -> Stack) into ((Stack, Interpreter) -> Stack)."
   [f]
-  (fn [s interpret]
-    (f s)))
+  (recursive
+    (fn [s interpret] (f s))))
+
+(defn value
+  "Helper for creating value functions."
+  [x]
+  (pure (fn [s] (conj s x))))
 
 (defn unop
   "Unary operator wrapper."
@@ -18,6 +29,12 @@
   "Binary operator wrapper."
   [f]
   (pure (fn [[x y & s]] (conj s (f x y)))))
+
+(defn native
+  "Helper for building core functions from Fifth code."
+  [code]
+  (recursive
+    (fn [s interpret] (interpret s code))))
 
 (def primops
   {'noop   (pure identity)
@@ -41,12 +58,21 @@
    '>=     (binop >=)
    '<      (binop <)
    '<=     (binop <=)
-   'map    (fn [[f xs & s] interpret]
-             (conj s (map (fn [x] (first (interpret (list x f)))) xs)))
-   'reduce (fn [[r i xs & s] interpret]
-             (conj s (reduce (fn [acc x] (first (interpret (list acc x r)))) i xs)))
-   'invoke (fn [[f & s] interpret]
-             (interpret s (list (symbol (name f)))))})
+   'map    (recursive
+             (fn [[f xs & s] interpret]
+               (conj s (map (fn [x] (first (interpret (list x f)))) xs))))
+   'reduce (recursive
+             (fn [[r i xs & s] interpret]
+               (conj s (reduce (fn [acc x] (first (interpret (list acc x r)))) i xs))))
+   'invoke (recursive
+             (fn [[f & s] interpret]
+               (interpret s (list (symbol (name f))))))
+   'store  (fn [[name x & s] interpreter scope] [s (assoc scope name (value x))])})
+
+(def prelude
+  {'filter (native '(:p store (reducer dup p invoke :conj :pop ? invoke) [] :reducer reduce))})
+
+(def stdlib (merge primops prelude))
 
 (defn term
   "Evaluate a term from command sequence with given scope."
@@ -55,14 +81,14 @@
     ; Symbols refer to functions in scope.
     (symbol? x) [(scope x) scope]
     ; Keywords are references to functions, not necessarily in scope.
-    (keyword? x) [(fn [s interpret] (conj s (symbol (name x)))) scope]
+    (keyword? x) [(fn [s interpret scope] [(conj s (symbol (name x))) scope]) scope]
     ; Lists denote function definitions. Head is function name.
-    (list? x) (let [[name & body] x] [(fn [s interpret] s)
+    (list? x) (let [[name & body] x] [(fn [s interpret scope] [s scope])
                                       (assoc scope
-                                        name (eval `(fn [s# interpret#]
-                                                      (interpret# s# '(~@body)))))])
+                                        name (eval `(fn [s# interpret# scope#]
+                                                      [(interpret# scope# s# '(~@body)) scope#])))])
     ; Anything else is just put onto the stack as a value.
-    :else [(fn [s interpret] (conj s x)) scope]))
+    :else [(fn [s interpret scope] [(conj s x) scope]) scope]))
 
 (defn interpret
   ([scope program]
@@ -70,8 +96,9 @@
   ([scope stack program]
    (let [[final fscope]
          (reduce (fn [[s scope] t]
+                   ; (println "term>>" t)
                    (let [[f scope'] (term t scope)]
-                     [(f s (partial interpret scope')) scope']))
+                     (f s interpret scope')))
                  [stack scope]
                  program)]
      final)))
@@ -81,7 +108,7 @@
     (clojure.walk/walk
       ->vec
       ->vec
-      (first (interpret primops prog)))))
+      (first (interpret stdlib prog)))))
 
 (defn run [] (fifth ((keep-odd dup even? not :conj :pop ? invoke)
                      [1 2 3 4 5] [] :keep-odd reduce)))
